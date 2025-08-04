0b7a96b57f749762055c28c26265e993c2bdf3d8
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2025-08-04 19:53:10 +0600

    Captcha is configured.

--- Plugins/Nop.Plugin.Widgets.CustomerQuery/Controllers/CustomerQueryController.cs
+++ Plugins/Nop.Plugin.Widgets.CustomerQuery/Controllers/CustomerQueryController.cs
@@ -1,36 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Nop.Core;
+using Nop.Core.Domain.Security;
 using Nop.Plugin.Widgets.CustomerQuery.Domain;
 using Nop.Plugin.Widgets.CustomerQuery.Models.Public;
 using Nop.Plugin.Widgets.CustomerQuery.Services;
+using Nop.Services.Localization;
 using Nop.Web.Controllers;
 
 namespace Nop.Plugin.Widgets.CustomerQuery.Controller;
 public class CustomerQueryController: BasePublicController
 {
     private readonly ICustomerQueryService _customerQueryService;
-   
+
+    protected readonly CaptchaSettings _captchaSettings;
+    protected readonly ILocalizationService _localizationService;
+
 
     public CustomerQueryController(
         ICustomerQueryService customerQueryService,
-        IWorkContext workContext)
+        IWorkContext workContext , CaptchaSettings captchaSettings,
+        ILocalizationService localizationService)
     {
-        _customerQueryService = customerQueryService;       
+        _captchaSettings = captchaSettings;
+        _customerQueryService = customerQueryService;
+        _localizationService = localizationService;
     }
 
     public IActionResult Index()
     {
         var model = new SubmitQueryModel();
+        model.DisplayCaptcha = true;
         // return View(  model);
         return View("~/Plugins/Widgets.CustomerQuery/Views/CustomerQuery/Index.cshtml", model);
 
     }
 
     [HttpPost]
-    public async Task<IActionResult> Index(SubmitQueryModel model)
+    public async Task<IActionResult> Index(SubmitQueryModel model, bool captchaValid)
     {
         if (!ModelState.IsValid)
             return View("Index", model);
+
+        //validate CAPTCHA
+        if (_captchaSettings.Enabled && _captchaSettings.ShowOnContactUsPage && !captchaValid)
+        {
+            ModelState.AddModelError("", await _localizationService.GetResourceAsync("Common.WrongCaptchaMessage"));
+        }
         // Binding to Save to database
         var query = new CustomerQueryRecord
         { 
